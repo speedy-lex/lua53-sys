7af9753b9613b40c322b627a2e35e3ba1bb9f38d
Speedy_Lex <78314533+speedy-lex@users.noreply.github.com>
2025-07-25 12:26:09 +0200

    implement alloc functions

---
+++ src/libc/alloc.rs
@@ -0,0 +1,40 @@
+use core::ffi::c_void;
+use core::alloc::Layout;
+use std::alloc;
+
+fn align_up(size: usize, align: usize) -> usize {
+    (size + align - 1) & !(align - 1)
+}
+
+#[unsafe(no_mangle)]
+unsafe extern "C" fn malloc(size: usize) -> *mut c_void {
+    unsafe {
+        let layout = Layout::from_size_align_unchecked(align_up(size + size_of::<usize>(), 16), 16);
+        let ptr = alloc::alloc(layout);
+        ptr.cast::<usize>().write(size + size_of::<usize>());
+        ptr.byte_offset(size_of::<usize>() as isize).cast()
+    }
+}
+#[unsafe(no_mangle)]
+unsafe extern "C" fn realloc(ptr: *mut c_void, new: usize) -> *mut c_void {
+    unsafe {
+        if ptr.is_null() {
+            return malloc(new);
+        }
+        let ptr = ptr.offset(-(size_of::<usize>() as isize));
+        let size = ptr.cast::<usize>().read();
+        let layout = Layout::from_size_align_unchecked(align_up(size, 16), 16);
+        let ptr = alloc::realloc(ptr.cast(), layout, new + size_of::<usize>());
+        ptr.cast::<usize>().write(new + size_of::<usize>());
+        ptr.byte_offset(size_of::<usize>() as isize).cast()
+    }
+}
+#[unsafe(no_mangle)]
+unsafe extern "C" fn free(ptr: *mut c_void) {
+    unsafe {
+        let ptr = ptr.offset(-(size_of::<usize>() as isize));
+        let size = ptr.cast::<usize>().read();
+        let layout = Layout::from_size_align_unchecked(size, 16);
+        alloc::dealloc(ptr.cast(), layout);
+    }
+}
--- src/libc/mod.rs
+++ src/libc/mod.rs
@@ -1,2 +1,3 @@
+mod alloc;
 mod ctype;
 mod math;
