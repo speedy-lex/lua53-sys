22aec54adcef948c144ccf27c7c1d50aaf226431
Speedy_Lex <78314533+speedy-lex@users.noreply.github.com>
2025-07-26 12:58:16 +0200

    implement calloc

--- src/libc/alloc.rs
+++ src/libc/alloc.rs
@@ -16,6 +16,16 @@ unsafe extern "C" fn malloc(size: usize) -> *mut c_void {
     }
 }
 #[unsafe(no_mangle)]
+unsafe extern "C" fn calloc(num: usize, size: usize) -> *mut c_void {
+    let size = num * size;
+    unsafe {
+        let layout = Layout::from_size_align_unchecked(align_up(size + size_of::<usize>(), 16), 16);
+        let ptr = alloc::alloc_zeroed(layout);
+        ptr.cast::<usize>().write(size + size_of::<usize>());
+        ptr.byte_offset(size_of::<usize>() as isize).cast()
+    }
+}
+#[unsafe(no_mangle)]
 unsafe extern "C" fn realloc(ptr: *mut c_void, new: usize) -> *mut c_void {
     unsafe {
         if ptr.is_null() {
