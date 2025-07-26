6252218ba44d5c212e6a75471e4b21033a270cab
Speedy_Lex <78314533+speedy-lex@users.noreply.github.com>
2025-07-26 12:48:21 +0200

    implement __cxa_end_catch

--- libc/libcpp_throw.cpp
+++ libc/libcpp_throw.cpp
@@ -77,6 +77,13 @@ void _Unwind_RaiseException(_Unwind_Exception *exception_object) {
   __builtin_wasm_throw(0, exception_object);
 }
 
+/// Called by __cxa_end_catch.
+void _Unwind_DeleteException(_Unwind_Exception *exception_object) {
+    // if (exception_object->exception_cleanup != NULL)
+    // (*exception_object->exception_cleanup)(_URC_FOREIGN_EXCEPTION_CAUGHT,
+    //                                        exception_object);
+}
+
 // Round s up to next multiple of a.
 static inline size_t aligned_allocation_size(size_t s, size_t a) {
     return (s + a - 1) & ~(a - 1);
@@ -85,6 +92,34 @@ static inline size_t aligned_allocation_size(size_t s, size_t a) {
 static inline size_t cxa_exception_size_from_exception_thrown_size(size_t size) {
     return aligned_allocation_size(size + sizeof (__cxa_exception),
                                    alignof(__cxa_exception));
+}
+
+//  This does not need to be atomic
+static inline int incrementHandlerCount(__cxa_exception *exception) {
+    return ++exception->handlerCount;
+}
+
+//  This does not need to be atomic
+static inline  int decrementHandlerCount(__cxa_exception *exception) {
+    return --exception->handlerCount;
+}
+
+static bool isDependentException(_Unwind_Exception* unwind_exception) {
+    return false; // uhh we cant track this and lua dont have so trust me bro
+}
+
+void __cxa_decrement_exception_refcount(void *thrown_object) throw() {
+    if (thrown_object != NULL )
+    {
+        __cxa_exception* exception_header = static_cast<__cxa_exception*>(thrown_object) - 1;
+        
+        if (--exception_header->referenceCount == 0)
+        {
+            if (NULL != exception_header->exceptionDestructor)
+                exception_header->exceptionDestructor(thrown_object);
+            __cxa_free_exception(thrown_object);
+        }
+    }
 }
 
 extern "C" {
@@ -162,5 +197,69 @@ __cxa_begin_catch(void* unwind_arg) throw()
     // Push the foreign exception on to the stack
     globals->caughtExceptions = exception_header;
     return unwind_exception + 1;
+}
+
+void __cxa_end_catch() {
+    __cxa_eh_globals* globals = __cxa_get_globals_fast(); // __cxa_get_globals called in __cxa_begin_catch
+    __cxa_exception* exception_header = globals->caughtExceptions;
+    // If we've rethrown a foreign exception, then globals->caughtExceptions
+    //    will have been made an empty stack by __cxa_rethrow() and there is
+    //    nothing more to be done.  Do nothing!
+    if (NULL != exception_header)
+    {
+        bool native_exception = true;//__isOurExceptionClass(&exception_header->unwindHeader);
+        if (native_exception)
+        {
+            // This is a native exception
+            if (exception_header->handlerCount < 0)
+            {
+                //  The exception has been rethrown by __cxa_rethrow, so don't delete it
+                if (0 == incrementHandlerCount(exception_header))
+                {
+                    //  Remove from the chain of uncaught exceptions
+                    globals->caughtExceptions = exception_header->nextException;
+                    // but don't destroy
+                }
+                // Keep handlerCount negative in case there are nested catch's
+                //   that need to be told that this exception is rethrown.  Don't
+                //   erase this rethrow flag until the exception is recaught.
+            }
+            else
+            {
+                // The native exception has not been rethrown
+                if (0 == decrementHandlerCount(exception_header))
+                {
+                    //  Remove from the chain of uncaught exceptions
+                    globals->caughtExceptions = exception_header->nextException;
+                    // Destroy this exception, being careful to distinguish
+                    //    between dependent and primary exceptions
+                    if (isDependentException(&exception_header->unwindHeader))
+                    {
+                        // ts shouldnt be possible with lua's code
+                        std::terminate();
+                        // // Reset exception_header to primaryException and deallocate the dependent exception
+                        // __cxa_dependent_exception* dep_exception_header =
+                        //     reinterpret_cast<__cxa_dependent_exception*>(exception_header);
+                        // exception_header =
+                        //     cxa_exception_from_thrown_object(dep_exception_header->primaryException);
+                        // __cxa_free_dependent_exception(dep_exception_header);
+                    }
+                    // Destroy the primary exception only if its referenceCount goes to 0
+                    //    (this decrement must be atomic)
+                    __cxa_decrement_exception_refcount(static_cast<void*>(exception_header + 1));
+                }
+            }
+        }
+        else
+        {
+            // The foreign exception has not been rethrown.  Pop the stack
+            //    and delete it.  If there are nested catch's and they try
+            //    to touch a foreign exception in any way, that is undefined
+            //     behavior.  They likely can't since the only way to catch
+            //     a foreign exception is with catch (...)!
+            _Unwind_DeleteException(&globals->caughtExceptions->unwindHeader);
+            globals->caughtExceptions = 0;
+        }
+    }
 }
 }
