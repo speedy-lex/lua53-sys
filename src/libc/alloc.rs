ab5049d72fa75ad1fccb0f09677af87cf87de480
Speedy_Lex <78314533+speedy-lex@users.noreply.github.com>
2025-07-29 22:13:42 +0200

    fix alloc bugs

--- src/libc/alloc.rs
+++ src/libc/alloc.rs
@@ -12,7 +12,7 @@ unsafe extern "C" fn malloc(size: usize) -> *mut c_void {
         let layout = Layout::from_size_align_unchecked(align_up(size + size_of::<usize>(), 16), 16);
         let ptr = alloc::alloc(layout);
         ptr.cast::<usize>().write(size + size_of::<usize>());
-        ptr.byte_offset(size_of::<usize>() as isize).cast()
+        ptr.byte_add(size_of::<usize>()).cast()
     }
 }
 #[unsafe(no_mangle)]
@@ -22,7 +22,7 @@ unsafe extern "C" fn calloc(num: usize, size: usize) -> *mut c_void {
         let layout = Layout::from_size_align_unchecked(align_up(size + size_of::<usize>(), 16), 16);
         let ptr = alloc::alloc_zeroed(layout);
         ptr.cast::<usize>().write(size + size_of::<usize>());
-        ptr.byte_offset(size_of::<usize>() as isize).cast()
+        ptr.byte_add(size_of::<usize>()).cast()
     }
 }
 #[unsafe(no_mangle)]
@@ -31,18 +31,21 @@ unsafe extern "C" fn realloc(ptr: *mut c_void, new: usize) -> *mut c_void {
         if ptr.is_null() {
             return malloc(new);
         }
-        let ptr = ptr.offset(-(size_of::<usize>() as isize));
+        let ptr = ptr.byte_sub(size_of::<usize>());
         let size = ptr.cast::<usize>().read();
         let layout = Layout::from_size_align_unchecked(align_up(size, 16), 16);
         let ptr = alloc::realloc(ptr.cast(), layout, new + size_of::<usize>());
         ptr.cast::<usize>().write(new + size_of::<usize>());
-        ptr.byte_offset(size_of::<usize>() as isize).cast()
+        ptr.byte_add(size_of::<usize>()).cast()
     }
 }
 #[unsafe(no_mangle)]
 unsafe extern "C" fn free(ptr: *mut c_void) {
+    if ptr.is_null() {
+        return;
+    }
     unsafe {
-        let ptr = ptr.offset(-(size_of::<usize>() as isize));
+        let ptr = ptr.byte_sub(size_of::<usize>());
         let size = ptr.cast::<usize>().read();
         let layout = Layout::from_size_align_unchecked(size, 16);
         alloc::dealloc(ptr.cast(), layout);
